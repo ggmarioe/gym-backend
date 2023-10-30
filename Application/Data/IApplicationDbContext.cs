05ba782277bebf27ebb574daabfb2051a23e13dc
Mario Guajardo <mario.e.guajardo.g@gmail.com>
2023-10-29 22:35:29 -0300

    20231029-fluentValidation-implementation
    - implements FluentValidation on Dependency Injection following the tutorial

---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Application.Data
+{
+    internal interface IApplicationDbContext
+    {
+        DbSet<Customer> Customers { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellation = default);
+
+    }
+}
