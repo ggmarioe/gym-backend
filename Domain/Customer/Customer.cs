8b2aa9c7ad094c499ddd4db0ec3c864410d4e548
Mario Guajardo <mario.e.guajardo.g@gmail.com>
2023-11-01 21:51:15 -0300

    2023111- Customer configuration
    - Fixed typo on Customer Email
    - Added customer configuration on the domain project

--- Domain/Customer/Customer.cs
+++ Domain/Customer/Customer.cs
@@ -13,7 +13,8 @@ namespace Domain.Customer
         public CustomerId Id { get; private set; }  
         public string Name { get; private set; } = string.Empty;
         public string LastName { get; private set; } = string.Empty;
-        public string Emial { get; private set; } = string.Empty;   
+        public string FullName => $"{Name} {LastName}";
+        public string Email { get; private set; } = string.Empty;   
         public PhoneNumber PhoneNumber { get; private set; }
         public Address Address { get; private set; }
         public bool IsActive { get; set; }
@@ -21,12 +22,12 @@ namespace Domain.Customer
 
         private Customer() { }
 
-        public Customer(CustomerId id, string name, string lastName, string emial, PhoneNumber phoneNumber, Address address, bool isActive)
+        public Customer(CustomerId id, string name, string lastName, string email, PhoneNumber phoneNumber, Address address, bool isActive)
         {
             Id = id;
             Name = name;
             LastName = lastName;
-            Emial = emial;
+            Email = email;
             PhoneNumber = phoneNumber;
             Address = address;
             IsActive = isActive;
